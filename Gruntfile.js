b4801fb02bea9bc30e7635f381ac331797e6ce97
Roma <vlasiukrv@gmail.com>
2018-10-13 12:09:18 +0300

    [rec-keep] Env:  Edit grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,15 @@
 module.exports = function (grunt) {
-    //описываем конфигурацию
-    grunt.config.init({
-        pkg: grunt.file.readJSON('package.json'), //подгружаем package.json, чтобы использовать его данные
 
-        jshint: {     // описываем как будет проверять наш код - jsHint
+    var build_Tasks = [
+        'concat',
+        'uglify',
+        'removelogging'
+    ];
+
+    grunt.config.init({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
             options: {
                 curly: true,
                 eqeqeq: true,
@@ -21,75 +27,82 @@ module.exports = function (grunt) {
                     console: true
                 }
             },
-            'taskList': {
+            test: {
                 src: 
                 	['src/main/js/**/*.js']
             }
         },
 
         clean: {
-            js: ['src/main/resources/static/js/build.min.js', 'src/main/resources/static/js/build.js']
+            build: [
+                'src/main/resources/static/js/*.js'
+            ]
         },
 
-        concat: {  //описываем работу плагина конкатенации
+        concat: {
             options: {
                 stripBanners: true,
                 banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                '<%= grunt.template.today("yyyy-mm-dd hh mm ss") %> */',
+                '<%= grunt.template.today("yyyy-mm-dd hh:mm:ss") %> */'
             },
-            dist: {
-                src: 
-                [
-            		'src/main/js/app/app-utils.js',
+            build: {
+                files: {
+                    'src/main/resources/static/js/custom.js': [
+                        'src/main/js/template-service.js'
+                    ],
+                    'src/main/resources/static/js/build.js': [
+                        'src/main/js/app/app-utils.js',
 
-            		'src/main/js/app/app-directive.js',
-            		'src/main/js/app/forms/app-directive-forms.js',
+                        'src/main/js/app/app-directive.js',
+                        'src/main/js/app/forms/app-directive-forms.js',
 
-            		'src/main/js/app/app-service.js',
-            		'src/main/js/app/forms/app-service-forms.js',
-            		'src/main/js/app/domain/app-service-model-enum.js',
-            		'src/main/js/app/domain/user/app-service-model-userl.js',
-            		'src/main/js/app/domain/project/app-service-model-projectl.js',
-            		'src/main/js/app/domain/task/app-service-model-taskl.js',
-            		'src/main/js/app/domain/farm/app-service-model-farm.js',
+                        'src/main/js/app/app-service.js',
+                        'src/main/js/app/forms/app-service-forms.js',
+                        'src/main/js/app/domain/app-service-model-enum.js',
+                        'src/main/js/app/domain/user/app-service-model-userl.js',
+                        'src/main/js/app/domain/project/app-service-model-projectl.js',
+                        'src/main/js/app/domain/task/app-service-model-taskl.js',
+                        'src/main/js/app/domain/farm/app-service-model-farm.js',
 
-            		'src/main/js/app/app-controller.js',
-            		'src/main/js/app/domain/user/app-controller-user.js',
-            		'src/main/js/app/domain/project/app-controller-project.js',
-            		'src/main/js/app/domain/task/app-controller-task.js',
-            		'src/main/js/app/domain/farm/app-controller-farm.js',
+                        'src/main/js/app/app-controller.js',
+                        'src/main/js/app/domain/user/app-controller-user.js',
+                        'src/main/js/app/domain/project/app-controller-project.js',
+                        'src/main/js/app/domain/task/app-controller-task.js',
+                        'src/main/js/app/domain/farm/app-controller-farm.js',
 
-            		'src/main/js/app/app.js'
+                        'src/main/js/app/app.js'
 
-                ],  // какие файлы конкатенировать
-                dest: 'src/main/resources/static/js/build.js'  // куда класть файл, который получиться после процесса конкатенации
+                    ]
+                }
             }
         },
 
-        uglify: {  //описываем работу плагина минификации js - uglify.
+        uglify: {
             options: {
                 stripBanners: true,
-                banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n' //комментарий который будет в минифицированном файле.
+                banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd hh:mm:ss") %> */'
             },
 
             build: {
-                src: 'src/main/resources/static/js/build.js',  // какой файл минифицировать
-                dest: 'src/main/resources/static/js/build.min.js' // куда класть файл, который получиться после процесса минификации
+                files: {
+                    'src/main/resources/static/js/build.min.js': ['src/main/resources/static/js/build.js'],
+                    'src/main/resources/static/js/custom.min.js': ['src/main/resources/static/js/custom.js']
+                }
             }
         },
 
-        watch: { //описываем работу плагина слежки за файлами.
-            scripts: {
-                files: ['src/main/js/*.js'],  //следить за всеми js файлами в папке src
-                tasks: ['jshint', 'concat', 'uglify', 'removelogging']  //при их изменении запускать следующие задачи
+        watch: {
+            build: {
+                files: ['src/main/js/**/*.js'],
+                tasks: build_Tasks
             }
         },
 
 
-        removelogging: { //описываем работу плагина удаления логов
-            dist: {
-                src: "src/main/resources/static/build.min.js", // файл который надо отчистить от console.log
-                dest: "src/main/resources/static/build.clean.js" // выходной файл, который получим после очистки
+        removelogging: {
+            build: {
+                src: "src/main/resources/static/build.min.js",
+                dest: "src/main/resources/static/build.clean.js"
             }
         }
 
@@ -105,6 +118,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-remove-logging');
 
     //регистрируем задачу
-    grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify', 'removelogging', 'watch']); //задача по умолчанию, просто grunt
-    grunt.registerTask('test', ['']); //пока пусто, но кто знает, возможно в следующих уроках мы этим воспользуемся :)
+
+    grunt.registerTask('build', build_Tasks);
+
+    grunt.registerTask('test', [
+        'jshint'
+    ]);
 };
