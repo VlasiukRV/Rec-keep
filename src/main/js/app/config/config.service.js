90e4c29661302852e859c46a9a2510c0a3451a87
VlasiukRV <vlasiukrv@gmail.com>
2019-07-25 21:28:21 +0300

    - refactor

--- src/main/js/app/config/config.service.js
+++ src/main/js/app/config/config.service.js
@@ -105,20 +105,207 @@
         };
 
         appEnvironment
-            .setMetadataSet(metadataSet)
+        .setMetadataSet(metadataSet)
 
-            .addMetadataEnumSpecification(metadataEnumSpecification_TaskState)
-            .addMetadataEntitySpecification(metadataEntitySpecification_Project)
-            .addMetadataEntitySpecification(metadataEntitySpecification_User)
-            .addMetadataEntitySpecification(metadataEntitySpecification_Role)
-            .addMetadataEntitySpecification(metadataEntitySpecification_ServiceTask)
-            .addMetadataEntitySpecification(metadataEntitySpecification_Task)
-            .addMetadataEntitySpecification(metadataEntitySpecification_Farm)
-            .addMetadataEntitySpecification(metadataEntitySpecification_PoultryCalendar)
+        .addMetadataEnumSpecification(metadataEnumSpecification_TaskState)
+        .addMetadataEntitySpecification(metadataEntitySpecification_Project)
+        .addMetadataEntitySpecification(metadataEntitySpecification_User)
+        .addMetadataEntitySpecification(metadataEntitySpecification_Role)
+        .addMetadataEntitySpecification(metadataEntitySpecification_ServiceTask)
+        .addMetadataEntitySpecification(metadataEntitySpecification_Task)
+        .addMetadataEntitySpecification(metadataEntitySpecification_Farm)
+        .addMetadataEntitySpecification(metadataEntitySpecification_PoultryCalendar)
 
-            .initMetadataSet();
+        .initMetadataSet();
         
         return appEnvironment;
     };
 
-})(window);
+    moduleConfig.metadataEntitySpecification_PoultryCalendar = function (Entity, fmListForm_TYPES) {
+
+        var PoultryCalendar = appUtils.Class(Entity);
+
+        return {
+            entityClass: PoultryCalendar,
+            fnGetEntityInstance: function () {
+                return new PoultryCalendar();
+            },
+            metadataName: 'poultryCalendar',
+            metadataRepresentation: 'poultryCalendar',
+            metadataDescription: 'Poultry calendar',
+            entityField: {
+                objectField: {},
+                entityField: {
+
+                        // entity field
+                        date: {
+                            value: '',
+                            fieldDescription: {
+                                inputType: 'date',
+                                label: 'Date',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        mortality: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Mortality',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        eggProduction: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Egg Production',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        eggCoolerDirtyFlats: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Dirty Flats',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        eggCoolerTemperatureHi: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Egg Cooler temperature Hi',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        eggCoolerTemperatureLo: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Egg Cooler temperature Lo',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        eggCoolerHumidity: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Egg Cooler humidity',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        barnTemperatureHi: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Barn temperature Hi',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        barnTemperatureLo: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Barn temperature Lo',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        waterMeterRead: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Water Meter Read',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        waterCons: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Water Cons',
+                                availability: true,
+                                entityListService: null
+                            }
+                        },
+                        feedCons: {
+                            value: 0,
+                            fieldDescription: {
+                                inputType: 'number',
+                                label: 'Feed Cons',
+                                availability: true,
+                                entityListService: null
+                            }
+                        }
+
+                    },
+                    defineField: {
+
+                        representation: {
+                            enumerable: true,
+                            get: function () {
+                                return '' + this.date;
+                            }
+                        }
+
+                    }
+                },
+
+                fmListForm: {
+                    listType: fmListForm_TYPES.table
+                },
+                entityFieldsPlacing: [
+                    [
+                    {editFieldId: 'id', fieldLength: 3},
+                    {
+                        editFieldId: [
+                        [{editFieldId: 'date', fieldLength: 12}]
+                        ],
+                        fieldLength: 5
+                    }
+                    ],
+                    [
+                    {editFieldId: 'mortality', fieldLength: 3},
+                    {editFieldId: 'eggProduction', fieldLength: 3},
+                    ],
+                    [
+                    {editFieldId: 'eggCoolerDirtyFlats', fieldLength: 3},
+                    {editFieldId: 'eggCoolerTemperatureHi', fieldLength: 3},
+                    {editFieldId: 'eggCoolerTemperatureLo', fieldLength: 3},
+                    {editFieldId: 'eggCoolerHumidity', fieldLength: 3},
+                    ],
+                    [
+                    {editFieldId: 'barnTemperatureHi', fieldLength: 3},
+                    {editFieldId: 'barnTemperatureLo', fieldLength: 3},
+                    ],
+                    [
+                    {editFieldId: 'waterMeterRead', fieldLength: 3},
+                    {editFieldId: 'waterCons', fieldLength: 3},
+                    {editFieldId: 'feedCons', fieldLength: 3},
+                    ],
+                    [
+                    {editFieldId: 'description', fieldLength: 12}
+                    ]
+                ]
+            };
+        };
+
+        moduleConfig.metadataEnumSpecification_TaskState = function (Enum) {        
+            var EnumTaskState = new Enum();
+
+            return {
+                enumClass: EnumTaskState,
+                metadataName: 'taskState'
+            };
+        }
+
+    })(window);
